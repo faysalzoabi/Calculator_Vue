cd53729780ef8ac86377bf8e27cc0552162b0bf2
Faysal AlZoubi <faysal.zoabi@gmail.com>
2019-01-24 01:02:09 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Calculator/>
   </div>
 </template>
+
+<script>
+import Calculator from './components/Calculator'
+export default {
+  components:{
+    Calculator
+  }
+}
+</script>
 
 <style>
 #app {
